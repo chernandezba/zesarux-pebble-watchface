8966a74d49c2ebf53e6456becac622f14554edd1
chernandezba <chernandezba@hotmail.com>
2025-11-25 23:48:35 +0100

    add background logo

--- src/c/zesarux-watchface.c
+++ src/c/zesarux-watchface.c
@@ -4,6 +4,9 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static BitmapLayer *s_background_layer;
+
+static GBitmap *s_background_bitmap;
 
 static void update_time() {
   // Get a tm structure
@@ -24,6 +27,16 @@ static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+  // Create GBitmap
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+
+  // Create BitmapLayer to display the GBitmap
+  s_background_layer = bitmap_layer_create(bounds);
+
+  // Set the bitmap onto the layer and add to the window
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+
   // Create the TextLayer with specific bounds
   s_time_layer = text_layer_create(
       GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
@@ -42,6 +55,12 @@ static void main_window_load(Window *window) {
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
+
+  // Destroy BitmapLayer
+  bitmap_layer_destroy(s_background_layer);
+
+  // Destroy GBitmap
+  gbitmap_destroy(s_background_bitmap);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -51,6 +70,9 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 static void init() {
   // Create main Window element and assign to pointer
   s_main_window = window_create();
+
+  // Change the background color
+  //window_set_background_color(s_main_window, GColorBlack);
 
   // Set handlers to manage the elements inside the Window
   window_set_window_handlers(s_main_window, (WindowHandlers) {
