900569f944f6de03e0b0d8bf22ec99ab9cb8cb9e
chernandezba <chernandezba@hotmail.com>
2025-11-29 00:32:33 +0100

    improve backgrounds

--- src/c/zesarux-watchface.c
+++ src/c/zesarux-watchface.c
@@ -48,9 +48,8 @@ static void update_time() {
 
   // Write the current hours and minutes into a buffer
   static char s_buffer[8];
-  //strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-  //                                        "%H:%M" : "%I:%M", tick_time);
-  strftime(s_buffer, sizeof(s_buffer), "%H:%M", tick_time);
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+                                          "%H:%M" : "%I:%M", tick_time);
 
   id_background = tick_time->tm_min;
 
