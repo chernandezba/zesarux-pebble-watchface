d0a565ac8d97bf65fc01d008794acee83db561fb
chernandezba <chernandezba@hotmail.com>
2025-12-12 00:57:57 +0100

    add config files

---
+++ src/pkjs/config.js
@@ -0,0 +1,29 @@
+module.exports = [
+  {
+    "type": "heading",
+    "defaultValue": "App Configuration"
+  },
+  {
+    "type": "text",
+    "defaultValue": "ZEsarUX watchface settings."
+  },
+  {
+    "type": "section",
+    "items": [
+      {
+        "type": "heading",
+        "defaultValue": "Behaviour"
+      },
+      {
+        "type": "toggle",
+        "messageKey": "Animations",
+        "label": "Animate every minute",
+        "defaultValue": true
+      }
+    ]
+  },
+  {
+    "type": "submit",
+    "defaultValue": "Save Settings"
+  }
+];
